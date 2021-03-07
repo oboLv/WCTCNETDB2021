1ce661c1c53c1bae9409554e9cfa94c4818eda4a
Chris Beilfuss <cbeilfuss1@my.wctc.edu>
2021-03-07 03:50:22 -0600

    lots of changes

--- MovieAssignment/Program.cs
+++ MovieAssignment/Program.cs
@@ -7,7 +7,7 @@ namespace MovieAssignment
     {
         static void Main(string[] args)
         {
-            var movieID = new List<string>();
+            var movieID = new List<int>();
             var titles = new List<string>();
             var genres = new List<string>();
             var exit = false;
@@ -18,7 +18,7 @@ namespace MovieAssignment
             {
                 var line = reader.ReadLine();
                 var split = line.Split(',');
-                movieID.Add(split[0]);
+                movieID.Add(Convert.ToInt32(split[0]));
                 titles.Add(split[1]);
                 genres.Add(split[2]);
             }
@@ -69,7 +69,43 @@ namespace MovieAssignment
                 }
                 else if(choice == "2")
                 {
-
+                    Console.Clear();
+                    var newGenreList = new List<string>();
+                    System.Console.Write("Enter Title: ");
+                    var newTitle = Console.ReadLine();
+                    System.Console.WriteLine(newTitle);
+                    var newGenreBool = false;
+                    do
+                    {
+                        System.Console.Write("Enter genre: ");
+                        var newGenre = Console.ReadLine();
+                        System.Console.WriteLine(newGenre);
+                        newGenreList.Add(newGenre);
+                        System.Console.WriteLine("Add more genres? (Y/N)");
+                        var moreGenres = Console.ReadLine();
+                        if(moreGenres.ToUpper() == "Y")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            newGenreBool = true;
+                        }
+                    }while(!newGenreBool);
+                    var joinedGenres = String.Join("|", newGenreList.ToArray());
+                    var newID = 0;
+                    foreach(int i in movieID)
+                    {
+                        if(i > newID)
+                        {
+                            newID = i + 1;
+                        }
+                    }
+                    movieID.Add(newID);
+                    titles.Add(newTitle);
+                    genres.Add(joinedGenres);
+                    var readyAddition = $"{newID},{newTitle},{joinedGenres}";
+                    var writer = new StreamWriter(file);
                 }
                 else if(choice == "3")
                 {
